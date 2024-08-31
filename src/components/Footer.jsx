6cb95050898038c48efc52f45fea4b3ed156047e
GHSHamad312 <ghshamad312@gmail.com>
2024-08-31 20:37:26 +0500

    fixed icons not showing

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,9 +7,9 @@ const Footer = () => {
             <div className="footertitle">Todo app Made by Hamad Ali Shah</div>
             <div className="contact">
             <div className="icons">
-                <a href=""><img  src="src/assets/github-142-svgrepo-com.svg" alt="github icon" /></a>
-                <a href=""><img src="src/assets/linkedin-svgrepo-com.svg" alt="linkedin icon" /></a>
-                <a href=""><img src="src/assets/gmail-svgrepo-com.svg" alt="gmail icon" /></a>
+                <a href=""><img  src="./src/assets/github-142-svgrepo-com.svg" alt="github icon" /></a>
+                <a href=""><img src="./src/assets/linkedin-svgrepo-com.svg" alt="linkedin icon" /></a>
+                <a href=""><img src="./src/assets/gmail-svgrepo-com.svg" alt="gmail icon" /></a>
             </div>
                 </div>
         </div>
