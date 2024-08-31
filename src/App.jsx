5df1a884e31ac16a3a583bd8c4baa694a860e255
GHSHamad312 <ghshamad312@gmail.com>
2024-08-31 16:34:08 +0500

    added storage and deployment

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,52 @@
-import './App.css'
-import { useState} from 'react'
-import Navbar from './components/Navbar'
-import Mainlist from './components/Mainlist'
-import Footer from './components/Footer'
-import { context } from './contexts/context'
+import './App.css';
+import { useState, useEffect } from 'react';
+import Navbar from './components/Navbar';
+import Mainlist from './components/Mainlist';
+import Footer from './components/Footer';
+import { context } from './contexts/context';
 
 function App() {
-  const [todo, settodo] = useState([])
-  const [number, setnumber]= useState(0)
+  const [todo, settodo] = useState([]);
+  const [number, setnumber] = useState(0);
+
+  useEffect(() => {
+
+    const storedTodos = localStorage.getItem("Todos");
+    if (storedTodos) {
+      const parsedTodos = JSON.parse(storedTodos);
+      console.log("Loaded todos from localStorage:", parsedTodos);
+      settodo(parsedTodos);
+    }
+
+
+    const storedNumber = localStorage.getItem("number");
+    if (storedNumber) {
+      const parsedNumber = parseInt(storedNumber, 10);
+      if (!isNaN(parsedNumber)) {
+        console.log("Loaded number from localStorage:", parsedNumber);
+        setnumber(parsedNumber);
+      }
+    } 
+  }, []);
+
+  useEffect(() => {
+ 
+    localStorage.setItem("Todos", JSON.stringify(todo));
+
+
+
+    localStorage.setItem("number", number.toString());
+  }, [todo, number]);
 
   return (
     <div className="main">
-    <context.Provider value={{todo , settodo , number, setnumber}}>
-      <Navbar />
-      <Mainlist />
+      <context.Provider value={{ todo, settodo, number, setnumber }}>
+        <Navbar />
+        <Mainlist />
       </context.Provider>
       <Footer />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -2,31 +2,35 @@ import './todo.css'
 import React, { useContext } from 'react'
 import { context } from '../contexts/context'
 
-const Todo = ( {value}) => {
-  const temp=useContext(context)
-  let handledel = ()=>{
-    
-    let temparr=temp.todo.filter((todo) => {return todo.id!==value.todo.id})
+const Todo = ({ value }) => {
+  const temp = useContext(context)
+  let handledel = () => {
+
+    let temparr = temp.todo.filter((todo) => { return todo.id !== value.todo.id })
     temp.settodo(temparr)
   }
-  let handleedit= ()=>{
+  let handleedit = () => {
     value.addref.current.value = value.todo.body
-    temp.todo[value.index].body=value.addref.current.value
+    temp.todo[value.index].body = value.addref.current.value
   }
-  let handlecomplete=()=>{
-    console.log(temp.todo[value.index])
-  temp.todo[value.index].completed=true
-  console.log(temp.todo[value.index])
+  let handlecomplete = () => {
+   let  updatedarr = temp.todo.map(element=>{
+      if(element.id==value.todo.id){
+       element.completed=true;
+    }
+    return element
+    })
+    temp.settodo(updatedarr)
   }
 
   return (
     <div className='todoparent'>
-      <div className="title">TODO #{value.index+1}</div>
+      <div className="title">TODO #{value.index + 1}</div>
       <div className="body">{value.todo.body}</div>
       <div className="controls">
-      <button className='edit' onClick={handleedit}>Edit </button>
-      <button className='delete'onClick={handledel}>Delete</button>
-      <button className='complete'onClick={handlecomplete}>Done</button>
+        <button className='edit' onClick={handleedit}>Edit </button>
+        <button className='delete' onClick={handledel}>Delete</button>
+        <button className='complete' onClick={handlecomplete}>Done</button>
       </div>
     </div>
   )
